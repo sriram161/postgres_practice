1c23267ead44f4f655ff48c0818bdb3993518dfb
sriram161 <k.sriramsagar@gmail.com>
2019-06-15 00:29:39 -0400

    still working on aggregations.

--- basics_2.sql
+++ basics_2.sql
@@ -4,3 +4,15 @@ select count(recommendedby), recommendedby from members
 where recommendedby is not null
 group by recommendedby
 order by recommendedby
+
+select facid, sum(slots) from bookings
+group by facid
+order by facid
+
+
+select date_part('month', starttime) from bookings
+
+select facid, sum(slots) from bookings
+where date_part('month', starttime) = 9 and date_part('year', starttime) = 2012
+group by facid
+order by sum(slots)
